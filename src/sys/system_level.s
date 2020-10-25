e48655957fc229241f3c659dc86bc4b665e6d066
tuba <danieemil@hotmail.com>
2020-10-25 18:21:09 +0100

    optimizaciones sobre level factory

--- src/sys/system_level.s
+++ src/sys/system_level.s
@@ -81,102 +81,86 @@ _sl_generate_level:
                 gl_check_enemy_turtle:
                 cp #GROUP_TURTLE
                 jr nc, gl_check_enemy_saw
-                    ;; Enemigo tortuga(ya... claro)
+                    ;; Enemigo tortuga
                     gl_check_enemy_turtle_right:
                     jr nz, gl_check_enemy_turtle_left
-                        ld a, #ET_TURTLE
                         ld de, #0x0100
-                        call _me_add_enemy
-                        jp gl_next_tile
+
+                        jr gl_generate_enemy_turtle
 
                     gl_check_enemy_turtle_left:
-                        ld a, #ET_TURTLE
                         ld de, #0xFF00
                         inc b
                         inc b
-                        call _me_add_enemy
 
-                        jp gl_next_tile
+                    gl_generate_enemy_turtle:
+                        ld a, #ET_TURTLE
+                        jp gl_generate_enemy
 
                 gl_check_enemy_saw:
                 cp #GROUP_SAW
-                jr nc, gl_check_enemy_rock
+                jp nc, gl_check_enemy_rock
                     ;; Enemigo sierra
                     gl_check_enemy_saw_right:
                     jr nz, gl_check_enemy_saw_right_down
-                        ld a, #ET_SAW
                         ld de, #0x0100
-                        call _me_add_enemy
-                        jp gl_next_tile
+                        
+                        jr gl_generate_enemy_saw
 
                     gl_check_enemy_saw_right_down:
                     cp #SAW_RIGHT_DOWN
                     jr nz, gl_check_enemy_saw_down
-                        ld a, #ET_SAW
                         ld de, #0x0101
-                        call _me_add_enemy
 
-                        jp gl_next_tile
+                        jr gl_generate_enemy_saw
 
                     gl_check_enemy_saw_down:
                     cp #SAW_DOWN
-                    jr nz, gl_check_enemy_saw_left_down
-                        ld a, #ET_SAW
-                        ld de, #0x0001
-                        call _me_add_enemy
-
-                        jp gl_next_tile
-
-                    gl_check_enemy_saw_left_down:
-                    cp #SAW_LEFT_DOWN
-                    jr nz, gl_check_enemy_saw_left
-                        ld a, #ET_SAW
-                        ld de, #0xFF01
-                        inc b
-                        inc b
-                        call _me_add_enemy
-
-                        jp gl_next_tile
-
-                    gl_check_enemy_saw_left:
-                    cp #SAW_LEFT
-                    jr nz, gl_check_enemy_saw_left_up
-                        ld a, #ET_SAW
-                        ld de, #0xFF00
-                        inc b
-                        inc b
-                        call _me_add_enemy
-
-                        jp gl_next_tile
-
-                    gl_check_enemy_saw_left_up:
-                    cp #SAW_LEFT_UP
                     jr nz, gl_check_enemy_saw_up
-                        ld a, #ET_SAW
-                        ld de, #0xFFFF
-                        inc b
-                        inc b
-                        call _me_add_enemy
+                        ld de, #0x0001
 
-                        jp gl_next_tile
+                        jr gl_generate_enemy_saw
+
 
                     gl_check_enemy_saw_up:
                     cp #SAW_UP
                     jr nz, gl_check_enemy_saw_right_up
-                        ld a, #ET_SAW
                         ld de, #0x00FF
-                        call _me_add_enemy
 
-                        jp gl_next_tile
+                        jr gl_generate_enemy_saw
 
                     gl_check_enemy_saw_right_up:
                     cp #SAW_RIGHT_UP
-                    jp nz, gl_next_tile
-                        ld a, #ET_SAW
+                    jp nz, gl_check_enemy_saw_left_down
                         ld de, #0x01FF
-                        call _me_add_enemy
 
-                        jp gl_next_tile
+                        jr gl_generate_enemy_saw
+
+                    gl_check_enemy_saw_left_down:
+                    inc b
+                    inc b
+                    cp #SAW_LEFT_DOWN
+                    jr nz, gl_check_enemy_saw_left
+                        ld de, #0xFF01
+
+                        jr gl_generate_enemy_saw
+
+                    gl_check_enemy_saw_left:
+                    cp #SAW_LEFT
+                    jr nz, gl_check_enemy_saw_left_up
+                        ld de, #0xFF00
+
+                        jr gl_generate_enemy_saw
+
+                    gl_check_enemy_saw_left_up:
+                    cp #SAW_LEFT_UP
+                    jr nz, gl_next_tile
+                        ld de, #0xFFFF
+
+
+                    gl_generate_enemy_saw:
+                        ld a, #ET_SAW
+                        jr gl_generate_enemy
 
                 gl_check_enemy_rock:
                 cp #GROUP_ROCK
@@ -184,19 +168,23 @@ _sl_generate_level:
                     ;; Enemigo roca
                     gl_check_enemy_rock_right:
                     jr nz, gl_check_enemy_rock_left
-                        ld a, #ET_ROCK
                         ld de, #0x0100
-                        call _me_add_enemy
-                        jp gl_next_tile
+                        jr gl_generate_enemy_rock
 
                     gl_check_enemy_rock_left:
-                        ld a, #ET_ROCK
                         ld de, #0xFF00
                         inc b
                         inc b
-                        call _me_add_enemy
 
-                        jp gl_next_tile
+                    gl_generate_enemy_rock:
+                        ld (hl), #0x00
+                        ld a, #ET_ROCK
+                        jr gl_generate_enemy
+
+            gl_generate_enemy:
+            ld (hl), #0x00
+            call _me_add_enemy
+            jp gl_next_tile
 
 
             gl_check_interactable:
@@ -204,41 +192,41 @@ _sl_generate_level:
             jr nc, gl_next_tile
                 ;; Interactuables
 
-
                 gl_check_interactable_checkpoint:
                 jr nz, gl_check_interactable_double_jump
+                    ld (hl), #0x01
                     ld a, #EI_CHECKPOINT
-                    call _mi_add_interactable
-                    jp gl_next_tile
+                    jr gl_generate_interactable
 
                 gl_check_interactable_double_jump:
                 cp #ID_DOUBLE_JUMP
                 jr nz, gl_check_interactable_gravity_up
+                    ld (hl), #0x00
                     ld a, #EI_DOUBLE_JUMP
-                    call _mi_add_interactable
-                    jp gl_next_tile
+                    jr gl_generate_interactable
 
                 gl_check_interactable_gravity_up:
                 cp #ID_GRAVITY_UP
                 jr nz, gl_check_interactable_gravity_down
+                    ld (hl), #0x00
                     ld a, #EI_GRAVITY_UP
-                    call _mi_add_interactable
-                    jp gl_next_tile
+                    jr gl_generate_interactable
 
                 gl_check_interactable_gravity_down:
                 cp #ID_GRAVITY_DOWN
                 jr nz, gl_check_interactable_collectable
+                    ld (hl), #0x00
                     ld a, #EI_GRAVITY_DOWN
-                    call _mi_add_interactable
-                    jp gl_next_tile
+                    jr gl_generate_interactable
 
                 gl_check_interactable_collectable:
                 cp #ID_COLLECTABLE
                 jp nz, gl_next_tile
+                    ld (hl), #0x00
                     ld a, #EI_COLLECTABLE
-                    call _mi_add_interactable
-                    jp gl_next_tile
 
+        gl_generate_interactable:
+            call _mi_add_interactable
 
         gl_next_tile:
         
